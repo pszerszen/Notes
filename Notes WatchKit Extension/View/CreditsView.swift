276acbc22e03bb9a789f713fc00cdf07087ac066
Piotr Szerszeń <piotr.b.szerszen@icloud.com>
2021-11-17 14:25:42 +0100

    CreditsView

---
+++ Notes WatchKit Extension/View/CreditsView.swift
@@ -0,0 +1,43 @@
+//
+//  CreditsView.swift
+//  Notes WatchKit Extension
+//
+//  Created by Piotr Szerszeń on 17/11/2021.
+//
+
+import SwiftUI
+
+struct CreditsView: View {
+
+    @State private var randomNumber = Int.random(in: 1...3)
+
+    private var randomImage: String {
+        return "developer-no\(randomNumber)"
+    }
+
+    var body: some View {
+        VStack {
+            Image(randomImage)
+                .resizable()
+                .scaledToFit()
+            .layoutPriority(1)
+
+            HeaderView(title: "Credits")
+
+            Text("Piotr Szerszeń")
+                .fontWeight(.bold)
+                .foregroundColor(.primary)
+
+            Text("Developer")
+                .font(.footnote)
+                .fontWeight(.light)
+                .foregroundColor(.secondary)
+        }
+    }
+}
+
+struct CreditsView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreditsView()
+    }
+}
